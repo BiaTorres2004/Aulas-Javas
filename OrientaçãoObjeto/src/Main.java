c277b0fe427bdaa706a37b32e1477002763598f0
aluno <aluno@DSN1251017043>
2022-04-04 11:34:00 -0300

    Commit Quarto

---
+++ OrientaçãoObjeto/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pessoa pessoa1 = new Pessoa(); 
+	    
+		pessoa1.nome = "Beatriz";
+		pessoa1.numFigurinhas = 1;
+		
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.numFigurinhas);
+	
+			
+	}
+
+}
