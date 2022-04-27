24e62fa69fad5128d67bfe6230968524f2e77314
aluno <aluno@DSN1251017043>
2022-04-27 11:39:57 -0300

    Metodo private

---
+++ Exerício/src/Lampada.java
@@ -0,0 +1,16 @@
+
+public class Lampada {
+	
+       private boolean ligada; 
+       private 
+       
+        
+       
+       
+       
+       
+       
+       
+       
+       
+                      }
