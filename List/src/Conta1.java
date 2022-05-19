0ff1c8419ba688f23cc3e5bfc201037cc95834d8
Beatriz Torres <102963239+BiaTorres2004@users.noreply.github.com>
2022-05-19 11:40:11 -0300

    Add files via upload

---
+++ List/src/Conta1.java
@@ -0,0 +1,24 @@
+
+public class Conta1 {
+
+      String nome; 
+      double valor;
+	public Conta1(String nome, double valor) {
+		
+		this.nome = nome;
+		this.valor = valor;
+	}
+	@Override
+	public String toString() {
+		return "Conta1 [nome=" + nome + ", valor=" + valor + "]";
+	}
+
+
+
+
+
+
+
+
+
+}
