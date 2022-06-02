3b15e0dca2440e0e23c4ee53d6b28386ae1d02ac
Beatriz Torres <102963239+BiaTorres2004@users.noreply.github.com>
2022-06-02 08:00:41 -0300

    Primeiro exercício

---
+++ TrocarLâmpada/src/Lamapada.java
@@ -0,0 +1,13 @@
+
+public class Lamapada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá mundo");
+		System.out.println("Gire a lâmpada em sentindo anti-horário");
+		System.out.println("Retire a lâmpada do bocal");
+		System.out.println("Coloque a lâmpada nova em sentindo horário");
+
+	} 
+
+}
