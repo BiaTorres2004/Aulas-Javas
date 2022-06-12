689af6f5d60a20493ec0c2fabb39df8c143f8113
Bzt_2004 <102963239+BiaTorres2004@users.noreply.github.com>
2022-06-12 20:20:01 -0300

    Update and rename Conta Bancaria/src/ConanBancaria.java to Conta Bancária/src/ConanBancaria.java

--- Conta Bancária/src/ConanBancaria.java
+++ Conta Bancária/src/ConanBancaria.java
@@ -1,11 +1,11 @@
-
+/*main*/
 public class ConanBancaria {
 	
 	private int numConta;
 	private double saldo;
 	private boolean ativo;
 	
-	//Isso faz com que a inicialização tenha um valor predefinido
+	//Isso faz com que a inicializaÃ§Ã£o tenha um valor predefinido
 	public ConanBancaria() {
 		numConta = 1 ;
 		saldo = 1.0;
@@ -22,23 +22,23 @@ public class ConanBancaria {
 
 
 
-	//Método de receber
+	//MÃ©todo de receber
 	public void Receber(double valor){
 		saldo += valor;
 		
 	}
 	
-	//Método de Ver
+	//MÃ©todo de Ver
 	public double Ver() {
 		return saldo;
 	}
 	
-	//Método de Dar
+	//MÃ©todo de Dar
 	public void Dar(double valor) {
 		if (saldo >= valor) {
 		saldo -= valor;
 		} else {
-			System.out.println("Você não tem dinheiro suficiente na conta!");
+			System.out.println("VocÃª nÃ£o tem dinheiro suficiente na conta!");
 		}
 	}
 	
