278185c29dcb6ce5f1a940d00732d22240e7a995
Bzt_2004 <102963239+BiaTorres2004@users.noreply.github.com>
2022-06-12 19:50:10 -0300

    Update CalculadoradeArea.java

--- CalcularArea/src/CalculadoradeArea.java
+++ CalcularArea/src/CalculadoradeArea.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
+/*Para que serve o scanner? O scanner serve para separar a entrada dos texto em blocos*/
 
+import java.util.Scanner;
 public class CalculadoradeArea {
 	public static void main(String[] args) {
 		
@@ -13,25 +14,25 @@ public class CalculadoradeArea {
 		
 		
 		System.out.println("1-Quadrado Largura 15 e altura 15");
-		System.out.println("2-Tri‚ngulo Largura 7.5 e altura 10.15");
-		System.out.println("3-Ret‚ngulo Largura 15 e Altura 10");
+		System.out.println("2-Tri√¢ngulo Largura 7.5 e altura 10.15");
+		System.out.println("3-Ret√¢ngulo Largura 15 e Altura 10");
 		System.out.println("Digite a figura escolhida 1,2 ou 3");
 		nome = entrada.nextInt();
 		
 
 		if(nome == 1) {
 		int area = sistema.CalArea(15, 15);
-		System.out.println("A ¿rea do Quadrado È " + area);
+		System.out.println("A ≈îrea do Quadrado √© " + area);
 		}
 		
 		else if(nome == 2 ) {
 		double area = sistema.CalArea(7.5, 10.15);
-		System.out.println("A ¿rea do Tri„ngulo È " + area);
+		System.out.println("A ≈îrea do TriƒÉngulo √© " + area);
 			}
 		
 		else {
 		int area = sistema.CalArea(15, 10);
-		System.out.println("A ¿rea do Ret‚ngulo È " + area);
+		System.out.println("A ≈îrea do Ret√¢ngulo √© " + area);
 		}
 			
 				
