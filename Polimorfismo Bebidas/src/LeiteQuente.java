bb7e88750a5ba545ef1604ca4b4f6c7ed22ad2ac
Bzt_2004 <102963239+BiaTorres2004@users.noreply.github.com>
2022-06-12 22:37:33 -0300

    Update and rename PolimorfismoBebidas/src/LeiteQuente.java to Polimorfismo Bebidas/src/LeiteQuente.java

--- Polimorfismo Bebidas/src/LeiteQuente.java
+++ Polimorfismo Bebidas/src/LeiteQuente.java
@@ -1,9 +1,11 @@
+/*Classe filha*/ 
 public class LeiteQuente extends Bebida{
 
 	public LeiteQuente () {
 		super ("Leite Quente ", false); 
 }
-	
+
+/*Aqui ela vai sobreescrever o metodo preparar. Isso também acontece no refrigerante*/ 	
 	@Override
 	public void preparar() {
 		System.out.println("Colocando o leite no copo");
