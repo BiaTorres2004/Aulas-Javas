4d92cd3911cd5516c8d55417301d8716d693f38e
Bzt_2004 <102963239+BiaTorres2004@users.noreply.github.com>
2022-06-12 21:16:53 -0300

    Update Lamapada.java

--- TrocarLâmpada/src/Lamapada.java
+++ TrocarLâmpada/src/Lamapada.java
@@ -1,12 +1,15 @@
+/*Primero uso do java"/ 
 
 public class Lamapada {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ol· mundo");
-		System.out.println("Gire a l‚mpada em sentindo anti-hor·rio");
-		System.out.println("Retire a l‚mpada do bocal");
-		System.out.println("Coloque a l‚mpada nova em sentindo hor·rio");
+
+/*Pode ser escrito um texto atrav√©s dele*/ 
+		System.out.println("Ol√° mundo");
+		System.out.println("Gire a l√¢mpada em sentindo anti-hor√°rio");
+		System.out.println("Retire a l√¢mpada do bocal");
+		System.out.println("Coloque a l√¢mpada nova em sentindo hor√°rio");
 
 	} 
 
