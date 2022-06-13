79f0b55f945dfff85edc96944724ed8d32721ca5
Bzt_2004 <102963239+BiaTorres2004@users.noreply.github.com>
2022-06-12 22:50:03 -0300

    Update and rename PolimorfismoBebidas/src/Aplicacao.java to Polimorfismo Bebidas/src/Aplicacao.java

--- Polimorfismo Bebidas/src/Aplicacao.java
+++ Polimorfismo Bebidas/src/Aplicacao.java
@@ -2,9 +2,11 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
+/Aqui o preparador vai ser instaciado e isso também acontece no leite-quente e no refrigerante*/ 
 		Preparador preparador = new Preparador();
 		
 		LeiteQuente leiteQuente = new LeiteQuente();
+/Aqui ele está chamando o leite-quente no metodo preparar*/ 
 		preparador.prepararBebidas(leiteQuente);
 		
 		Refrigerante refrigerante = new Refrigerante();
