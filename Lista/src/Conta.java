8680bb7e81e36779840b62a6295bb1e08bd910ff
Bzt_2004 <102963239+BiaTorres2004@users.noreply.github.com>
2022-06-12 22:16:47 -0300

    Update and rename List/src/Conta.java to Lista/src/Conta.java

--- Lista/src/Conta.java
+++ Lista/src/Conta.java
@@ -4,13 +4,12 @@ public class Conta {
 	String nome;
 	double valor;
 
-//construting source
+/*Aqui a fonte estÃ¡ sendo construida*/ 
 	public Conta(String nome, double valor) {
 		this.nome = nome;
 		this.valor = valor;
 	}
-//to string
-//para não mostrar o endereço e sim o conteudo da variavel das contas
+/
 	@Override
 	public String toString() {
 		return "Conta [nome=" + nome + ", valor=" + valor + "]";
