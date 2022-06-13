51282a36cf05d9c5ce3570202788dba79952c41f
Bzt_2004 <102963239+BiaTorres2004@users.noreply.github.com>
2022-06-12 22:34:05 -0300

    Update Main.java

--- OrientaçãoObjeto/src/Main.java
+++ OrientaçãoObjeto/src/Main.java
@@ -2,11 +2,16 @@
 public class Main {
 
 	public static void main(String[] args) {
+/*Aqui Beatriz está iniciando com 1 figurinhas*/ 
 		Pessoa pessoa1 = new Pessoa(); 
-	    
-		pessoa1.nome = "Beatriz";
+	        pessoa1.nome = "Beatriz";
 		pessoa1.numFigurinhas = 1;
-		
+
+ /*Aqui sera usado o metodo receber e dar*/ 
+                pessoa1.receber(3); 
+                pessoa1.dar(3); 
+
+/*Aqui Vai mostrar no console a quantidade de figurinhas*/ 		
 		System.out.println(pessoa1.nome);
 		System.out.println(pessoa1.numFigurinhas);
 	
